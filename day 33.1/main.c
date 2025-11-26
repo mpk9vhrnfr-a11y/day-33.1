818ed20f26a31b9937e7a3e5d4d1d7328f2aeda4
sanaa <Sanaakumar07@gmail.com>
2025-11-26 12:15:18 +0530

    c

--- day 33.1/main.c
+++ day 33.1/main.c
@@ -1,15 +1,28 @@
-//
-//  main.c
-//  day 33.1
-//
-//  Created by Sanaa Kumar on 26/11/25.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main(void) {
+int a[100], b[100], c[200];
+int n1, n2, i, j;
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+    printf("Enter %d elements of first array:\n", n1);
+    for(i = 0; i < n1; i++) {
+        scanf("%d", &a[i]);
+    }
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+    printf("Enter %d elements of second array:\n", n2);
+    for(i = 0; i < n2; i++) {
+        scanf("%d", &b[i]);
+    }
+    for(i = 0; i < n1; i++) {
+        c[i] = a[i];
+    }
+    for(j = 0; j < n2; j++) {
+        c[i + j] = b[j];
+    }
+    printf("Merged array:\n");
+    for(i = 0; i < n1 + n2; i++) {
+        printf("%d ", c[i]);
+    }
+    return 0;
 }
